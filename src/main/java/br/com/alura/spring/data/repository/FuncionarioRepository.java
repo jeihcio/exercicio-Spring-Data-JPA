550eace6c61c55d7f0f2af67ac53176a7ab54705
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-03-26 19:34:52 -0300

    Adicionando projeção de funcionários

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
@@ -19,4 +20,7 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 
 	@Query(value = "Select * From funcionarios f where f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+
+	@Query(value = "select f.id, f.nome, f.salario from funcionarios f", nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionarioSalario();
 }
